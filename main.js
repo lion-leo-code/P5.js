516453dbf587f58fb85c15e888e22bb6bea03546
lion-leo-code <72197492+lion-leo-code@users.noreply.github.com>
2021-04-01 17:51:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function setup(){
+canvas = createCanvas(640, 480);
+canvas.position(450, 250);
+video = createCapture(VIDEO);
+video.hide();
+
+tint_color = "";
+}
+
+function draw(){
+    image(video, 0, 0, 640, 480);
+    tint(tint_color);
+}
+
+function take_snapshot(){
+    save('MyTintImage.png');
+}
+
+function filter_tint(){
+    tint_color = document.getElementById("color_name").value;
+}
